68b0406b81f3aeb31a1c167f426266a8709174c6
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-04-14 09:19:49 -1000

    creating login rider or driver selection screen

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -25,11 +25,45 @@ import com.parse.SaveCallback;
 
 public class MainActivity extends AppCompatActivity {
 
+  public void getStarted(View view){
+    Switch userTypeSwitch = (Switch) findViewById(R.id.userTypeSwitch);
+    Log.i("Switch Value", String.valueOf(userTypeSwitch.isChecked()));
+
+    String userType = "rider";
+
+    if(userTypeSwitch.isChecked()){
+      userType = "driver";
+    }
+
+    ParseUser.getCurrentUser().put("riderOrDriver", userType);
+
+    Log.i("Info", "Redirecting as " + userType);
+  }
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    getSupportActionBar().hide();
+
+    if(ParseUser.getCurrentUser() == null){
+      ParseAnonymousUtils.logIn(new LogInCallback() {
+        @Override
+        public void done(ParseUser user, ParseException e) {
+          if(e == null){
+            Log.i("Info", "Anonymous login successful");
+          }else{
+            Log.i("Info","Anonymous login failed");
+          }
+        }
+      });
+    }else{
+      if(ParseUser.getCurrentUser().get("riderOrDriver") != null){
+        Log.i("Info", "Redirecting as " + ParseUser.getCurrentUser().get("riderOrDriver"));
+      }
+    }
 
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
 
